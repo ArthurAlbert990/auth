2c9b156194e65f92fcf26af807e6adb65c3869f3
Arthur Albert <arthur.albert990@gmail.com>
2023-11-08 10:51:44 -0300

    Commit do projeto.

---
+++ README.md
@@ -0,0 +1,51 @@
+# -- Projeto: AUTH
+
+Autenticação utilizando o módulo passport + NodeJS, Express e MongoDB.
+
+
+## 🚀 Funcionamento
+
+O método de autenticação utilizado é do tipo "local" (módulo passport), salvando o usuário e senha no MongoDB.
+OBS: A senha é salva em hash através do módulo bcryptjs.
+
+A sessão/usuário ativo é salvo e mantido por middleware em app.js através do passport.
+Acesso em qualquer tela através da variável global: currentUser
+
+
+### 🔧 Instalação
+
+Para a instalação, basta abrir o terminal de comando na pasta raiz e executar:
+```
+npm install
+```
+
+Para configurar o banco de dados, crie um arquivo .env na raiz do diretório com conteúdo:
+```
+# Variáveis de Ambiente, MONGO DB
+MONGO_URI = <uri_do_mongo_db>
+```
+
+## ⚙️ Para executar:
+
+Para executar o projeto, no terminal, insira o comando:
+```
+nodemon app.js
+```
+
+Após, acesse a URL: localhost:3000
+
+
+## 🛠️ Construído com
+
+Mencione as ferramentas que você usou para criar seu projeto
+
+* [NodeJS](http://www.dropwizard.io/1.0.2/docs/)
+* [Express](https://expressjs.com/pt-br/) - Framework Web.
+* [MongoDB/Mongoose](https://mongoosejs.com/) - Banco de Dados.
+* [Passport](https://www.passportjs.org/) - Autenticação.
+* [Bcryptjs](https://www.npmjs.com/package/bcryptjs) - Gerar hash de senhas.
+
+
+## ✒️ Autor
+
+* **Arthur A.** - *Tudo* - [Arthur A.](https://github.com/arthuralbert990)
---
+++ app.js
@@ -0,0 +1,125 @@
+const express = require("express")
+const path = require('path')
+const session = require('express-session')
+const passport = require("passport")
+const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+require('dotenv').config()
+const bcrypt = require('bcryptjs')
+
+
+
+const mongoDb = (process.env.MONGO_URI)
+mongoose.connect(mongoDb)
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "mongo connection error"))
+
+db ? console.log('DB conectado.') : 'erro em db'
+
+const User = mongoose.model("User", new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+}))
+
+const app = express()
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.use(session({ secret: 'dogs', resave: false, saveUninitialized: true }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.urlencoded({ extended: false }))
+
+
+//AUTENTICAÇÃO
+//inicializa passport / AUTENTICAÇÃO
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username })
+            if (!user) {
+                return done(null, false, { message: "Usuário incorreto" })
+            }
+            const match = await bcrypt.compare(password, user.password)
+            if (!match) {
+                return done(null, false, { message: "Senha incorreta" })
+            }
+
+            return done(null, user)
+
+        } catch (err) {
+            return done(err)
+        }
+    })
+)
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id)
+        done(null, user)
+    } catch (err) {
+        done(err)
+    }
+})
+
+//MIDDLEWARE PARA DAR ACESSO AO currentUser a TODAS AS VIEWS
+// NÃO SENDO NECESSÁRIO PASSAR MANUALMENTE:
+//pode ser acessador através da variável currentUser
+app.use((req, res, next) => {
+    try {
+        res.locals.currentUser = req.user;
+        next()
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
+
+//ROTAS ROTAS ROTAS ROTAS...
+app.get('/', (req, res) => {
+    res.render('index', { user: req.user })
+})
+
+app.get('/sign-up', (req, res) => res.render('sign-up-form'))
+
+app.post('/sign-up', async (req, res, next) => {
+    try {
+        //hash da senha, cria usuario
+        bcrypt.hash(req.body.password, 10, async (err, hash) => {
+            if (err) { next(err) }
+            else {
+                const user = new User({
+                    username: req.body.username,
+                    password: hash,
+                })
+                const result = await user.save()
+                console.log('NOVO USUARIO: ' + result)
+                res.redirect('/')
+            }
+        })
+    } catch (err) {
+        return next(err)
+    }
+})
+
+app.post('/log-in', passport.authenticate('local',
+    {
+        successRedirect: '/',
+        failureRedirect: '/'
+    }));
+
+app.get('/log-out', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/')
+    })
+})
+
+
+
+app.listen(3000, () => console.log('App executando na porta 3000.'))
